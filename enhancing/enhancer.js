a20d2b521ba0374870f746ab8bd0780dd4f412de
Chanis Torres <chanis_torres@yahoo.com>
2021-01-16 21:23:43 -0500

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,26 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
-}
+  if(item.enhancement < 20) {
+    return {...item, enhancement: item.enhancement + 1};
+  } else {
+    return item;
+  }
+};
 
 function fail(item) {
-  return { ...item };
+  const ie = item.enhancement
+  if( ie < 15 ) {
+    return { ...item, enhancement: ie - 5}
+  } else if ( ie >= 15 && ie <= 16) {
+    return { ...item, enhancement: ie -10 }
+  } else {
+    return { ...item, enhancement: ie - 1}
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,54 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+var items = [
+    { name: 'sword',
+    durability: 90,
+    enhancement: 15 },
+    { name: 'armor',
+    durability: 100,
+    enhancement: 20 },
+    { name: 'gun',
+    durability: 60,
+    enhancement: 13 }
+]
+
+describe('enhancer unit tests', () => {
+
+    it('repairs', () => {
+        expect(enhancer.repair(items[0]))
+        .toEqual({ name: 'sword', durability: 100, enhancement: 15 })
+
+        expect(enhancer.repair(items[1]))
+        .toEqual({ name: 'armor', durability: 100, enhancement: 20 })
+
+        expect(enhancer.repair(items[2]))
+        .toEqual({ name: 'gun', durability: 100, enhancement: 13 })
+    })
+
+    it('enhancement succeeds', () => {
+
+        expect(enhancer.success(items[0]))
+        .toEqual({ name: 'sword', durability: 90, enhancement: 16 })
+
+        expect(enhancer.success(items[1]))
+        .toEqual({ name: 'armor', durability: 100, enhancement: 20 })
+
+        expect(enhancer.success(items[2]))
+        .toEqual({ name: 'gun', durability: 60, enhancement: 14 })
+    })
+
+    it('enhancement fails', () => {
+
+        expect(enhancer.fail(items[0]))
+        .toEqual({ name: 'sword', durability: 90, enhancement: 5})
+
+        expect(enhancer.fail(items[2]))
+        .toEqual({ name: 'gun', durability: 60, enhancement: 8})
+
+        expect(enhancer.fail(items[1]))
+        .toEqual({ name: 'armor', durability: 100, enhancement: 19})
+
+    })
+
+})
